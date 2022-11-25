40ae244b571e9709ce14b05d45e73bf7ab3f81f8
grandsmarquis <jdourlens@gmail.com>
2022-11-25 19:08:47 +0900

    try fetch

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const fetch = require('node-fetch');
 const axios = require('axios');
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const port = 3000
 
 app.post('/*', async (req, res) => {
-    let result = await axios.post("https://rpc.flashbots.net/", req.body);
-    console.log( req.body)
-    res.json(result.data)
+
+    const response = await fetch('https://httpbin.org/post', {
+        method: 'post',
+        body: req.body,
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await response.json();
+    console.log(data)
+    return
+    let result = axios.post("https://rpc.flashbots.net/", req.body, {
+        headers: {
+            // Overwrite Axios's automatically set Content-Type
+            'Content-Type': 'application/json'
+        }
+    }).then(function (result) {
+        res.send(result);
+        return
+        console.log(result)
+        console.log(req.body)
+        res.setHeader('Content-Type', 'application/json');
+        console.log(result.data)
+        res.end(JSON.stringify(result.data));
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
 })
 
 app.listen(port, () => {
