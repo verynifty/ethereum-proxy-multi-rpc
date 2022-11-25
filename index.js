002db574b0109f8935b2e2a6c037809b81c55e85
grandsmarquis <jdourlens@gmail.com>
2022-11-25 19:14:08 +0900

    tx not good

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fetch = require('node-fetch');
 const axios = require('axios');
 
 const app = express()
@@ -11,24 +10,13 @@ const port = 3000
 
 app.post('/*', async (req, res) => {
 
-    const response = await fetch('https://httpbin.org/post', {
-        method: 'post',
-        body: req.body,
-        headers: { 'Content-Type': 'application/json' }
-    });
-    const data = await response.json();
-    console.log(data)
-    return
-    let result = axios.post("https://rpc.flashbots.net/", req.body, {
+    let result = axios.post("https://nodes.mewapi.io/rpc/eth", req.body, {
         headers: {
-            // Overwrite Axios's automatically set Content-Type
             'Content-Type': 'application/json'
         }
     }).then(function (result) {
-        res.send(result);
-        return
-        console.log(result)
         console.log(req.body)
+        console.log(result)
         res.setHeader('Content-Type', 'application/json');
         console.log(result.data)
         res.end(JSON.stringify(result.data));
